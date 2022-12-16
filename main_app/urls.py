871c8d2d7755a0aaa6cd82b51b72b032d76a255a
ronnepuzondev <116023509+ronnepuzondev@users.noreply.github.com>
2022-12-16 13:17:47 -0800

    finish lessons

---
+++ main_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  # route for cats index
+  path('cats/', views.cats_index, name='index'),
+  path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
+]
+
+
---
+++ main_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Cat
+
+# Define the home view
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, "cats/index.html", {"cats": cats})
+
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
+
+
+# Add the Cat class & list and view function below the imports
